a5ddc64fc10130ebfa0921ea376ee08423eb7f10
jccallo <jccallo@outlook.com>
2022-12-10 01:15:48 -0500

    chore: @VieChild para obtener valor de un input

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
-  templateUrl: './busqueda.component.html'
+  templateUrl: './busqueda.component.html',
 })
 export class BusquedaComponent {
-
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
+  search() {
+    const input = this.txtSearch.nativeElement;
+    console.log(input.value);
+    input.value = '';
+  }
 }
